fce67ee3d6ea79ac92ff616de6ff80a35ffc8eae
StevenCharny <steven@charny.co>
2021-09-21 03:20:29 -0500

    finished assignment, uploading to github

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log(17);
+//strings, numbers, bool, null, undefined
+//  +  -  *  /  **  %
+console.log(5%4);
+console.log(5%2);
+console.log('I\'M MARY POPPINS Y\'ALL!');
+console.log("Hawk" + "eye");
+console.log("Steven".length + "Charny".length);
+console.log("Avengers: Endgame"[16]);
+console.log("/");
+console.log(5**4-9);
+console.log("I".length - "Am".length + ("Iron".length % "Man".length));
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+var m=25, c=4, u=m+c;
+console.log(u);
